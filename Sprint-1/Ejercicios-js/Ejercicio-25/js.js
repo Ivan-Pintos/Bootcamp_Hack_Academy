18eaee7f3a182c513584692bc3cef747ea1d9a7d
IvanPintos <ProyectosIvanPintos@gmail.com>
2023-04-21 12:59:22 -0300

    Ejercicio - 25 Create my first Chart

--- Sprint-1/Ejercicios-js/Ejercicio-25/js.js
+++ Sprint-1/Ejercicios-js/Ejercicio-25/js.js
@@ -1,71 +1,36 @@
-import { CubejsApi } from "@cubejs-client/core";
-
-const apiUrl =
-  "https://heavy-lansford.gcp-us-central1.cubecloudapp.dev/cubejs-api/v1";
-const cubeToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjEwMDAwMDAwMDAsImV4cCI6NTAwMDAwMDAwMH0.OHZOpOBVKr-sCwn8sbZ5UFsqI3uCs6e4omT7P6WVMFw";
-
-const cubeApi = new CubejsApi(cubeToken, { apiUrl });
-
-export async function getAquisitionsByYear() {
-  const acquisitionsByYearQuery = {
-    dimensions: ["Artworks.yearAcquired"],
-    measures: ["Artworks.count"],
-    filters: [
-      {
-        member: "Artworks.yearAcquired",
-        operator: "set",
-      },
-    ],
-    order: {
-      "Artworks.yearAcquired": "asc",
+var datosChart = {
+  labels: [],
+  datasets: [
+    {
+      label: "PBI URUGUAY (USD)",
+      data: [],
+      backgroundColor: "rgba(0, 0, 0, 0.1)",
+      borderColor: "rgba(0, 0, 0, 0.3)",
+      borderWidth: 1,
+      fill: true,
+      tension: 0.5,
     },
-  };
-
-  const resultSet = await cubeApi.load(acquisitionsByYearQuery);
-
-  return resultSet.tablePivot().map((row) => ({
-    year: parseInt(row["Artworks.yearAcquired"]),
-    count: parseInt(row["Artworks.count"]),
-  }));
-}
-
-export async function getDimensions() {
-  const dimensionsQuery = {
-    dimensions: ["Artworks.widthCm", "Artworks.heightCm"],
-    measures: ["Artworks.count"],
-    filters: [
-      {
-        member: "Artworks.classification",
-        operator: "equals",
-        values: ["Painting"],
-      },
-      {
-        member: "Artworks.widthCm",
-        operator: "set",
-      },
-      {
-        member: "Artworks.widthCm",
-        operator: "lt",
-        values: ["500"],
-      },
-      {
-        member: "Artworks.heightCm",
-        operator: "set",
-      },
-      {
-        member: "Artworks.heightCm",
-        operator: "lt",
-        values: ["500"],
-      },
-    ],
-  };
-
-  const resultSet = await cubeApi.load(dimensionsQuery);
-
-  return resultSet.tablePivot().map((row) => ({
-    width: parseInt(row["Artworks.widthCm"]),
-    height: parseInt(row["Artworks.heightCm"]),
-    count: parseInt(row["Artworks.count"]),
-  }));
-}
+  ],
+};
+fetch(
+  "https://gist.githubusercontent.com/SuecoMarcus/23bcf8a786725f1b8335ec2dc2d90779/raw/f33d1058b878a7fc95ef202763d0067e8b436e54/uruguay_gdp.json"
+)
+  .then((data) => data.json())
+  .then((dataJson) => {
+    for (let index = dataJson.length - 1; index >= 0; index--) {
+      datosChart.labels.push(dataJson[index].date);
+      datosChart.datasets[0].data.push(dataJson[index].value);
+    }
+    var ctx = document.getElementById("myChart").getContext("2d");
+    new Chart(ctx, {
+      type: "line",
+      data: datosChart,
+      // options: {
+      //   scales: {
+      //     xAxis: {
+      //       reverse: true,
+      //     },
+      //   },
+      // },
+    });
+  });
