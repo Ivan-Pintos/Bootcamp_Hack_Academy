38266923daa3254cd9addfb3d62b14d61230505f
IvanPintos <ProyectosIvanPintos@gmail.com>
2023-04-28 18:17:26 -0300

    mejoras y actualizacion del ejercicio 14

--- Sprint-2/ejercicio_express_json/controllers/MainController.js
+++ Sprint-2/ejercicio_express_json/controllers/MainController.js
@@ -1,43 +1,48 @@
 const express = require("express");
-const teams_bd = require("../db");
+const { teams: teams_2 } = require("../db");
 
 module.exports = {
   teams: function teams(req, res) {
-    res.send(teams_bd.teams);
+    res.json(teams_2);
   },
   teams_post: function teams_post(req, res) {
-    teams_bd.teams[req.body.id] = req.body;
-    res.send("Estas en la pagina de teams a traves del metodo post");
+    const { id, name, flag } = req.body; // de esta forma podemos aumentar la seguridad sobre los datos que se nos envian
+    teams_2[id] = { id, name, flag };
+    console.log(req.body);
+    res.json(teams_2);
   },
   teams_id: function teams_id(req, res) {
-    const id = req.params.id.slic1e();
-    for (const teams_key in teams_bd.teams) {
-      if (id === teams_bd.teams[teams_key].id) {
-        res.send(teams_bd.teams[teams_key]);
-        return;
-      }
+    const id = req.params.id;
+    if (id === teams_2[id].id) {
+      res.json(teams_2[id]);
+      return;
     }
     res.send(`La busqueda del id ${id} no tiene resultados`);
   },
+
+  //Cambiar esto para que no envie un json, sino que se envie la id a traves de la URL
   teams_delete: function teams_delete(req, res) {
     const id = req.body.id;
-    for (const teams_key in teams_bd.teams) {
-      if (id === teams_bd.teams[teams_key].id) {
-        delete teams_bd.teams[teams_key];
-        res.send(`El team con la id: ${id} se ha eliminado exitosamenete`);
+    for (const teams_key in teams_2) {
+      if (id === teams_2[teams_key].id) {
+        s;
+        delete teams_2[teams_key];
+        res.json(teams_2);
         return;
       }
     }
     res.send(`El team con la id ${id} no existe`);
   },
+
+  // COmprobar que esto funciona en el caso de qe que solamente se envie un dato adicional en vez de los dos
   teams_patch: function teams_patch(req, res) {
     const id = req.body.id;
-    for (const teams_key in teams_bd.teams) {
-      if (id === teams_bd.teams[teams_key].id) {
-        teams_bd.teams[teams_key].name = req.body.name;
-        teams_bd.teams[teams_key].flag = req.body.flag;
-        console.log(teams_bd.teams[teams_key]);
-        res.send(`El team con la id: ${id} se ha editado exitosamenete`);
+    for (const teams_key in teams_2) {
+      if (id === teams_2[teams_key].id) {
+        teams_2[teams_key].name = req.body.name;
+        teams_2[teams_key].flag = req.body.flag;
+        console.log(teams_2[teams_key]);
+        res.json(teams_2);
         return;
       }
     }
--- Sprint-2/ejercicio_express_json/router.js
+++ Sprint-2/ejercicio_express_json/router.js
@@ -3,10 +3,10 @@ const controller = require("./controllers/MainController");
 const router = express.Router();
 
 router.use(express.urlencoded({ extended: true }));
-router.use(express.json());
+router.use(express.json()); // Esta linea permite añadir un objeto json que se envie a traves del metodo post
 
 router.get("/teams", controller.teams);
-router.get("/teams:id", controller.teams_id);
+router.get("/teams/:id", controller.teams_id);
 router.post("/teams", controller.teams_post);
 router.delete("/teams", controller.teams_delete);
 router.patch("/teams", controller.teams_patch);
